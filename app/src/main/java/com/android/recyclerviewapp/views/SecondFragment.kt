0cb1dab5d41149bc1a739f596a57ad7a3359776d
inaki92 <tred1992>
2022-03-13 20:08:44 -0400

    review

--- app/src/main/java/com/android/recyclerviewapp/views/SecondFragment.kt
+++ app/src/main/java/com/android/recyclerviewapp/views/SecondFragment.kt
@@ -13,16 +13,6 @@ import com.android.recyclerviewapp.model.Event
 import com.android.recyclerviewapp.model.EventSingleton
 import java.text.SimpleDateFormat
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SecondFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SecondFragment : Fragment() {
 
     private var dateParam: Long = 0
@@ -56,22 +46,19 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        binding.eventCalendar.setOnDateChangeListener(CalendarView.OnDateChangeListener {
-                calendarView, i, i2, i3 ->
+        binding.eventCalendar.setOnDateChangeListener { calendarView, i, i2, i3 ->
             val month = i2 + 1
-            if (month <= 9) {
+            formattedDate = if (month <= 9) {
                 if (i3 <= 9) {
-                    formattedDate = "0$month/0$i3/$i"
+                    "0$month/0$i3/$i"
+                } else {
+                    "0$month/$i3/$i"
                 }
-                else {
-                    formattedDate = "0$month/$i3/$i"
-                }
-            }
-            else {
-                formattedDate = "$month/$i3/$i"
+            } else {
+                "$month/$i3/$i"
             }
 
-        }).let {
+        }.let {
             sdf = SimpleDateFormat("mm/dd/yyyy")
             formattedDate = sdf.format(binding.eventCalendar.date)
         }
@@ -88,16 +75,6 @@ class SecondFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
         fun newInstance() = SecondFragment()
     }
 }
